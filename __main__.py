7a1e181249f3c6c12483347a054b3362bea3c7af
Jan Vlnas <git@jan.vlnas.cz>
2012-05-28 04:33:27 +0200

    handle program execution from cmd

--- __main__.py
+++ __main__.py
@@ -2,17 +2,29 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Jan Vlnas"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 import argparse
+import brainx
 
-parser = argparse.ArgumentParser(prog='pybrainx') #description=''
-parser.add_argument('file', help='a file to process')
+parser = argparse.ArgumentParser(prog='pybrainx', description='BrainFuck interpreter with BrainLoller and BrainCopter support') #description=''
+parser.add_argument('file', help='a file to process; text file for BrainFuck, PNG image for BrainLoller and BrainCopter')
 parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-l', action='store_true', help='handle file as brainloller program')
-group.add_argument('-c', action='store_true', help='handle file as braincopter program')
+group.add_argument('-l', '--brainloller', action='store_true', help='handle file as BrainLoller program (expects PNG image)')
+group.add_argument('-c', '--braincopter', action='store_true', help='handle file as BrainCopter program (expects PNG image)')
 
 args = parser.parse_args()
 
-print(args)
+
+if args.brainloller:
+    img = brainx.BrainLoller(args.file)
+    data = img.load()
+elif args.braincopter:
+    img = brainx.BrainCopter(args.file)
+    data = img.load
+else:
+    with open(args.file, encoding='ascii') as stream:
+        data = stream.read()
+program = brainx.BrainFuck(data, memory=b'\x00', output='', show_output=True)
+program.run()
